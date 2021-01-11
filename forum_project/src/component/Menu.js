bc91c928941cef604c744a0fd2fa0dcefc511db9
basinibi <johann1314@gmail.com>
2021-01-11 10:13:47 +0900

    base structure

---
+++ forum_project/src/component/Menu.js
@@ -0,0 +1,19 @@
+import '../css/Menu.css';
+
+function Menu() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>플젝하쉴?!</h1>
+				<nav>
+					<span>메뉴1</span>
+					<span>메뉴2</span>
+					<span>메뉴3</span>
+				</nav>
+				<img src='image/login.png'></img>
+      </header>
+    </div>
+  );
+}
+
+export default Menu;
