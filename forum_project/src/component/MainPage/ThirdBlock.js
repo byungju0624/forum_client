6a1f56a26f279e97f773ae7c8a4e7df26d170ba3
basinibi <johann1314@gmail.com>
2021-01-12 16:30:18 +0900

    complete main page ui

--- forum_project/src/component/MainPage/ThirdBlock.js
+++ forum_project/src/component/MainPage/ThirdBlock.js
@@ -2,7 +2,13 @@ import '../../css/MainPage/ThirdBlock.css';
 
 function ThirdBlock() {
   return (
-    <div className="App">
+    <div className="third_block">
+			<div className="regist_box">
+				<div className="regist_text">
+					프로젝트 등록하기
+				</div>
+				<img className="regist_img" src="image/regist.png"></img>
+			</div>
     </div>
   );
 }
