097b9a2cf8e6ba957b7a607b5cfab208f15655a1
정병주 <jeongbyeongju@jeongbyeongjuui-MacBookPro.local>
2021-01-16 12:15:20 +0900

    Guide add

--- forum_project/src/component/Guide/Guide.js
+++ forum_project/src/component/Guide/Guide.js
@@ -1,10 +1,30 @@
-import { useState } from "react";
-import "../../css/Guide/Guide.css";
-import { firestore } from "../../firebase";
-import { storage } from "../../firebase";
+import styles from "../../css/Guide/Guide.module.css";
 
 function Guide() {
-  return <div>Guide</div>;
+  return (
+    <div className={styles.header}>
+      <div className={styles.first}>
+        <h2>•First Step</h2>
+        <video
+          style={{
+            backgroundColor: "black",
+            marginLeft: "30px",
+          }}
+        ></video>
+        <h3>•설명</h3>
+      </div>
+      <div className={styles.second}>
+        <h2>•Second Step</h2>
+        <video style={{ backgroundColor: "black", marginLeft: "30px" }}></video>
+        <h3>•설명</h3>
+      </div>
+      <div className={styles.third}>
+        <h2>•Third Step</h2>
+        <video style={{ backgroundColor: "black", marginLeft: "30px" }}></video>
+        <h3>•설명</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Guide;
