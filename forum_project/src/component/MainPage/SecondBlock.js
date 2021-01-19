f581b478643e3b0d213a94f729446ce94583d4b1
basinibi <johann1314@gmail.com>
2021-01-20 02:42:33 +0900

    fix data duplication

--- forum_project/src/component/MainPage/SecondBlock.js
+++ forum_project/src/component/MainPage/SecondBlock.js
@@ -1,48 +1,37 @@
 import "../../css/MainPage/SecondBlock.css";
-
 import Slider from "react-slick";
-import React, { Component } from "react";
-const photos = [
-  {
-    name: "photo1",
-    url:
-      "http://img.khan.co.kr/news/2019/11/29/l_2019112901003607500286631.jpg",
-  },
-  {
-    name: "photo2",
-    url:
-      "https://www.ui4u.go.kr/depart/img/content/sub03/img_con03030100_01.jpg",
-  },
-  {
-    name: "photo3",
-    url: "https://image.dongascience.com/Photo/2018/01/15159739972169[1].jpg",
-  },
-  {
-    name: "photo4",
-    url:
-      "https://www.sisa-news.com/data/photos/20200936/art_159912317533_32480a.jpg",
-  },
-];
+import React, { useState } from "react";
+import { useEffect } from "react";
 
-export default class ThirdBlock extends Component {
-  render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      slidesToShow: 2,
-      slidesToScroll: 2,
-      arrow: true,
-      className: "slides",
-    };
-    return (
-      <div className="second">
-        <h2>프로젝트 열람</h2>
+let dataFire = JSON.parse(localStorage.getItem("fireStoreData"))
+
+function SecondBlock () {
+
+	useEffect(() => {
+		console.log("루삥뽕"+JSON.stringify(dataFire))
+	},[])
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    arrow: true,
+		className: "slides",
+	}
+
+  return (
+    <div className="second">
+      <h2>프로젝트 열람</h2>
         <Slider {...settings}>
-          {photos.map((photo) => {
-            return <img src={photo.url} className="img"></img>;
+          {dataFire.map((eachData) => {
+            return <img src={eachData.image} className="img"></img>;
           })}
         </Slider>
       </div>
     );
   }
-}
+
+
+
+export default SecondBlock
